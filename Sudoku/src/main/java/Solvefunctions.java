c85c0fc873d3c06b369ae4ef65f7e75626215aaf
latelaukki <aaltonenlauri1@gmail.com>
2020-03-29 18:47:44 +0300

    viikko3  first push

---
+++ Sudoku/src/main/java/Solvefunctions.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class Solvefunctions {
+    
+    public int[][] Solve(ArrayList<Integer> t) {
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+    
+    
+}
